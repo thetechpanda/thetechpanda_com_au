e6bb0d7c54892d7834e30c24f51b368fe3022bb1
thetechpanda <24541832+thetechpanda@users.noreply.github.com>
2026-02-28 13:57:48 +1100

    fix: tidy garbage.js

--- js/garbage.js
+++ js/garbage.js
@@ -1,17 +1,14 @@
-'use strict'
+'use strict';
 /**
  * this is garbage code.
  */
-// in my tests a simple rot that does not uses the standard 13 digits rotation is enough to keep the spammers at bay
-const email = '`e]f7k_\\k\\Z_gXe[X%Zfd%Xl';
-// this is unnecessary, but I am a nerd, what can I do?
-const abn = '.)/\'))*+,,0';
-// simple implementation of Cesar's cypher, rot13
-const cesar = (string, cesarC = 13) => {
+const email = '`e]f7k_\\k\\Z_gXe[X%Zfd%Xl'
+const abn = '.)/\'))*+,,0'
+const shiftChars = (string, shiftV = 13) => {
     let out = ''
     string = '' + string
     for (let c = 0; c < string.length; c++) {
-        out += String.fromCharCode(string.charCodeAt(c) + cesarC)
+        out += String.fromCharCode(string.charCodeAt(c) + shiftV)
     }
     return out
 }
@@ -30,27 +27,27 @@ const clipboard = src => {
 const hl = (elm, tip) => {
     elm.classList.toggle('inverted')
     tipbox(elm, tip)
-    if (  elm.classList.contains('inverted')  ) {
+    if (elm.classList.contains('inverted')) {
         return setTimeout(() => hl(elm), 2000)
     }
     return true
 }
 // shows/hide a tipbox, element is used only to don't clog the body with useless html elements
 const tipbox = (element, message) => {
-    if ( element.getElementsByTagName('tip').length > 0 ) {
-        for(let tipbox of element.getElementsByTagName('tip')) {
+    if (element.getElementsByTagName('tip').length > 0) {
+        for (let tipbox of element.getElementsByTagName('tip')) {
             element.removeChild(tipbox)
         }
         return
-    } 
+    }
     let tipbox = document.createElement('tip')
     tipbox.innerText = message
     element.appendChild(tipbox)
 }
 
 // dom ready function
-function ready(f){
-    if ( /in/.test(document.readyState) ) {
+function ready(f) {
+    if (/in/.test(document.readyState)) {
         requestAnimationFrame(() => ready(f))
     } else {
         f()
@@ -58,8 +55,8 @@ function ready(f){
 }
 // email@domain.tld => email [at] domain.tld
 let printmail = (string) => string.replace(/@/, '&#32;&#91;&#97;&#116;&#93;&#32;')
-// decode uses rot9
-let decode = (string) => cesar(string, 9)
+// decode uses shift char -9
+let decode = (string) => shiftChars(string, 9)
 
 ready(() => {
     for (let spamblock of document.querySelectorAll('spam')) {
@@ -71,6 +68,7 @@ ready(() => {
                 spamblock.onclick = () => {
                     let e = document.createElement('a')
                     e.href = 'mailto:' + decode(email)
+                    e.target = "_blank"
                     document.body.appendChild(e)
                     e.click()
                     document.body.removeChild(e)
@@ -83,8 +81,5 @@ ready(() => {
                 spamblock.onclick = () => clipboard(spamblock)
                 break
         }
-    }
-    for(let anchor of document.querySelectorAll('a') ) {
-        anchor.setAttribute('target', '_blank');
     }
 })
